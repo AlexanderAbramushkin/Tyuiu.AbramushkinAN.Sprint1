ae18e507fc16d9c8ae9dd87c96f858ef28a474d1
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-18 12:30:30 +0500

    Task2

---
+++ Tyuiu.AbramushkinAN.Sprint1.Task2.V20.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.AbramushkinAN.Sprint1.Task2.V20.Lib;
+
+namespace Tyuiu.AbramushkinAN.Sprint1.Task2.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            int y = 3;
+            var res = ds.CalculateSquaresSumm(x, y);
+            Assert.AreEqual(13, res);    
+        }
+    }
+}
---
+++ Tyuiu.AbramushkinAN.Sprint1.Task2.V20/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.AbramushkinAN.Sprint1.Task2.V20.Lib;
+namespace Tyuiu.AbramushkinAN.Sprint1.Task2.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #20                                                             *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Заданы два числа. Вычислить сумму их квадратов.                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.CalculateSquaresSumm(x, y));
+        }
+    }
+}
