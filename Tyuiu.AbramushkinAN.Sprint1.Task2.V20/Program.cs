5050788b7fd8e9f2229c82cfaae28ee1f9ba0ce7
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-18 12:56:47 +0500

    Task2

--- Tyuiu.AbramushkinAN.Sprint1.Task2.V20/Program.cs
+++ Tyuiu.AbramushkinAN.Sprint1.Task2.V20/Program.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.AbramushkinAN.Sprint1.Task2.V20
             Console.Title = "Спринт #1 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
-            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
             Console.WriteLine("* Задание #2                                                              *");
             Console.WriteLine("* Вариант #20                                                             *");
             Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
