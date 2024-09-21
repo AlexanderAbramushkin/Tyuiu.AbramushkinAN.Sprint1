4df0d936f073de8b9ddaeac3726a0bd56609ea60
Tyuiu.AbramushkinAN@outlook.com <Tyuiu.AbramushkinAN@outlook.com>
2024-09-21 20:02:28 +0500

    Task7

---
+++ Tyuiu.AbramushkinAN.Sprint1.Task7.V18/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.AbramushkinAN.Sprint1.Task7.V18.Lib;
+namespace Tyuiu.AbramushkinAN.Sprint1.Task7.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Абрамушкин А. Н. | ИИПб-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
+            Console.WriteLine("* Выполнил: Абрамушкин Александр Николаевич | ИИПб-24-2                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+            Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                            1+sin^2(x+y)                                 *");
+            Console.WriteLine("*                        z= ------------- +x                              *");
+            Console.WriteLine("*                           |      2x    |                                *");
+            Console.WriteLine("*                         2+|x- ---------|                                *");
+            Console.WriteLine("*                           |   1+x^2*y^2|                                *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x, y));
+        }
+    }
+}
